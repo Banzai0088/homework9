8af58e27a3939da195d66b868911ebb630cda2be
Alena <banzai0088@ya.ru>
2023-04-13 13:01:15 +0300

    добавили решения задач и файлы на отслеживание

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+//  	N = 5. -> "5, 4, 3, 2, 1"
+//  	N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
+ 	 
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// NaturalToLow(number, count);
+ 	 
+ 	 
+// void NaturalToLow(int n, int count)
+// {
+// if (count > n)
+// {
+// return;
+// }
+// else
+// {
+// NaturalToLow(n, count + 1);
+// Console.Write(count + " ");
+// }
+// }
+
+//Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+ 
+// Console.WriteLine("Введите число M:");
+// int M = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число N:");
+// int N = int.Parse(Console.ReadLine());
+ 	 
+// int sum = 0;
+ 	 
+// int PrintNumberSum()
+// {
+// if (M>N)
+// {
+// return M;
+// }
+// sum +=M;
+// M++;
+ 	 
+// return PrintNumberSum();
+// }
+ 	 
+// PrintNumberSum();
+// Console.WriteLine(sum);
+
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+ 	 
+// Console.WriteLine("Введите число M:");
+// int m = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число N:");
+// int n = int.Parse(Console.ReadLine());
+ 	 
+// AkkermanFunction(m,n);
+ 	 
+// void AkkermanFunction(int m, int n)
+// {
+// Console.Write(Akkerman(m, n));
+// }
+ 	 
+// int Akkerman(int m, int n)
+// {
+// if (m == 0)
+// {
+// return n + 1;
+// }
+// else if (n == 0 && m > 0)
+// {
+// return Akkerman(m - 1, 1);
+// }
+// else
+// {
+// return (Akkerman(m - 1, Akkerman(m, n - 1)));
+// }
+// }
